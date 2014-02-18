d750a8369e00d829d06802b9fc546351ccc78b8b
eikiu <eikiu.inc@gmail.com>
2014-02-18 11:20:18 -0300

    typos new way to filter twitter author

--- plugins/parse_mentions/parse_mentions.py
+++ plugins/parse_mentions/parse_mentions.py
@@ -17,7 +17,7 @@ class ParseMentions(Task):
 		
 		
 		yield self.group_task()
-		
+	
 		#load yaml to obj var in post.
 		for lang in kw["translations"]:
 			for post in kw['timeline']:
@@ -29,17 +29,35 @@ class ParseMentions(Task):
 				if os.path.exists(path_file) and os.path.isfile(path_file):		
 					with open (path_file, 'r') as yaml_file:
 						data_mentions = yaml.load(yaml_file)
-						if not 'webmention' in data_mentions:
-							data_mentions['webmention'] = {}
-						if not 'tweets' in data_mentions:
-							data_mentions['tweets'] = {}
+					if not 'webmention' in data_mentions:
+						data_mentions['webmention'] = {}
+					if not 'tweets' in data_mentions:
+						data_mentions['tweets'] = {}
 							
-					
-					
 					# sort by date, Newest first. 
-					
 					# Take advantage of twitter with saving old first. 
 					# Just reversing
-					#sorted(data_mentions['tweets'], reverse=True)				
+					#sorted(data_mentions['tweets'], reverse=True)	
+					
+					# merge all twits from author in one, using lasted url 
+					# and count all favs + Retweets. And remove from tweet list 
+					twitter_author = self.site.config['TWITTER_USER']
+
+					data_mentions['tweet_author'] = {}
+					favorite_count = 0
+					retweet_count  = 0
+					tweet_id       = ""
+
+					for tweet, prop in list(data_mentions['tweets'].items()):
+						if prop['user_screen_name'] == twitter_author:
+							favorite_count += prop['favorite_count'] 
+							retweet_count  += prop['retweet_count']
+							tweet_id = prop['id']
+
+							del data_mentions['tweets'][tweet]
+
+					data_mentions['tweet_author']['favorite_count'] = favorite_count
+					data_mentions['tweet_author']['retweet_count'] = retweet_count
+					data_mentions['tweet_author']['id'] = tweet_id
 
 					post.mentions = data_mentions
