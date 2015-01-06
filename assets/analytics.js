864e4f4e454e2854e788f53863ade78ffef1b807
eikiu <eikiu.inc@gmail.com>
2015-01-06 14:33:54 -0300

    update japanese resources

--- assets/analytics.js
+++ assets/analytics.js
@@ -3,8 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-47044833-1', 'quinzi.com.ar');
+ga('create', 'UA-47044833-1', 'aquinzi.com');
 ga('send', 'pageview');
-
-
-
